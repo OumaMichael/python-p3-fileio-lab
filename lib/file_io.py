65e824c4cb53ddad1aacf40886bb796b8078c8c1
MICHAEL OUMA <oumamichael108@gmail.com>
2025-05-22 17:02:33 +0300

    Update file_io.py

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,14 @@
 def write_file(file_name, file_content):
-    pass
+    full_name = f"{file_name}.txt"
+    with open(full_name, 'w') as f:
+        f.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    full_name = f"{file_name}.txt"
+    with open(full_name, 'a') as f:
+        f.write(append_content)
 
 def read_file(file_name):
-    pass
+    full_name = f"{file_name}.txt"
+    with open(full_name, 'r') as f:
+        return f.read()
